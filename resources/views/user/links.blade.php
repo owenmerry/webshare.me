5f050fc51c67599a3736c92fe2cd5a9474becc13
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2018-04-05 23:03:19 +0100

    added mobile pop up edit change

--- resources/views/user/links.blade.php
+++ resources/views/user/links.blade.php
@@ -34,7 +34,7 @@
                 <div class="image" ng-click="openlink(link.id,link.url)" style="background-image:url(@{{link.image}});">
                 </div>
                 <div class="text">
-                <h2 ng-click="openlink(link.id,link.url)" >@{{link.title | strLimit: 37}}</h2>
+                <h2 ng-click="openlink(link.id,link.url)" >@{{link.title | strLimit: 30}}</h2>
                 <h3>@{{link.site.name | strLimit: 20}}</h3>
                 <h3 class="views">@{{link.visits}} views</h3>
                     <div class="clearfix"></div>
